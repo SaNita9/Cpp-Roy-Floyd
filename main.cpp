86b1f38067aa2673447f70830e389cd93dd99205
SaNita9 <143259862+SaNita9@users.noreply.github.com>
2026-02-19 20:19:30 +0200

    comments

--- main.cpp
+++ main.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <limits.h>
 using namespace std;
 
 ifstream fin("roy-floyd.in");
 ofstream fout("roy-floyd.out");
 
+//p[i][j] represents the distance matrix
 int p[101][101];
-
 
 int main() {
   int m, n, i, j, x, y, z, k;
     
     fin>>n>>m;
-    for(i=1; i<=n; i++){
-        for(j=1; j<=n; j++)
-            p[i][j]=1001;
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++)
+            //all pairs start as "unreachable" (infinity)
+            p[i][j]=INT_MAX;
+        //distance from a node to itself is 0
         p[i][i]=0;
     }
-    
+
+    //read the edges
     for(i=1; i<=m; i++){
-        fin>>x>>y>>z;
+        fin >> x >> y >> z;
         p[x][y]=z;
     }
 
-
-  // for(i=1; i<=n; i++){
-  //     for(j=1; j<=n; j++)
-  //         cout<<p[i][j]<<' ';
-  //     cout<<endl;
-  // }
-  // cout<<"_________________"<<endl;
-
-  
-    for(k=1; k<=n; k++)
-        for(i=1; i<=n; i++)
-            for(j=1; j<=n; j++){
+  // ROY-FLOYD
+    for(k = 1; k<=n; k++)
+        for(i = 1; i <= n; i++)
+            for(j = 1; j<=n; j++){
                 if(i!=j)
-                    if(p[i][j]>p[i][k]+p[k][j])
-                        p[i][j]=p[i][k]+p[k][j];
+                    //check if going through an intermediate node yealds a shorter path
+                    if(p[i][j] > p[i][k] + p[k][j])
+                        p[i][j] = p[i][k] + p[k][j];
             }
-    
-  for(i=1; i<=n; i++){
-        for(j=1; j<=n; j++)
-            if(p[i][j]!=1001)fout<<p[i][j]<<' ';
-                else fout<<-1<<' ';
+  //output  
+  for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++)
+            if(p[i][j] != INT_MAX)
+              fout << p[i][j] << ' ';
+            else fout << -1 <<' ';
         fout<<endl;
     }
 }
